af641fa4ac97b20a16d48aca177270bea36c0204
Redjul2110 <julian.fischer1@t-online.de>
2026-02-13 20:38:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const assets = [
+    "image1.png",
+    "video1.mp4"
+];
 
+const gallery = document.getElementById("gallery");
+
+assets.forEach(file => {
+
+    const container = document.createElement("div");
+    container.className = "asset";
+
+    const extension = file.split('.').pop().toLowerCase();
+    let element;
+
+    if (["png","jpg","jpeg","gif","webp"].includes(extension)) {
+        element = document.createElement("img");
+        element.src = file;
+    } else {
+        element = document.createElement("video");
+        element.src = file;
+        element.controls = true;
+    }
+
+    container.appendChild(element);
+
+    const btn = document.createElement("button");
+    btn.innerText = "Copy URL";
+
+    btn.onclick = () => {
+        const url = window.location.origin + "/" + file;
+        navigator.clipboard.writeText(url);
+        alert("URL copied!");
+    };
+
+    container.appendChild(btn);
+    gallery.appendChild(container);
+});
